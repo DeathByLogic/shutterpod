9f29a192429f048fba06f2cc304eaca9af3de829
dwassenberg@gmail.com <dwassenberg@gmail.com@b7604f24-7041-182a-c031-293265fa7804>
2010-07-09 01:52:56 +0000

    Added initial commit of code from local files.

---
+++ firmware/src/screen_text.h
@@ -0,0 +1,23 @@
+/*  
+ *  File	: pin.cpp
+ *  Date	: 6/23/2010
+ *
+ *  Project	: Shutter Pod
+ *  Author	: D. Wassenberg
+ *  Purpose	: Menu and setting text strings
+ *
+ */
+ 
+// Spash screen
+char MSG_SPLASH_1[] =		"  Shutter  Pod  ";
+char MSG_SPLASH_2[] =		"  Version  0.1  ";
+
+// Main menu
+char MSG_MAIN_MENU[] =		"Main Menu";
+
+char MSG_SETTINGS_OPT[] =	"<-  Settings  ->";
+char MSG_TIME_LPS_OPT[] = 	"<- Time Lapse ->";
+char MSG_LONG_EXP_OPT[] =	"<- Long Expos ->";
+char MSG_MANNUAL_OPT[] =	"<-   Manual   ->";
+
+// Settings menu
